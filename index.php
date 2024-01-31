8581dc61a04603d57a1f2e788a3a1c534505f42c
oliverstasa <oliver.stasa@gmail.com>
2024-01-31 12:15:45 +0000

    redundancy fixes

--- index.php
+++ index.php
@@ -2,10 +2,9 @@
 <html>
 
 <head>
-    <script src="./js/index.js?v=<?php echo rand(0, 123123); ?>" type="module"></script>
-    <link href="./css/main.css?v=<?php echo rand(0, 123123); ?>" rel="stylesheet">
+    <script src="./js/index.js" type="module"></script>
+    <link href="./css/main.css" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="image_src" href="./data/bang.png" alt="bang">
 </head>
 
 <body>
